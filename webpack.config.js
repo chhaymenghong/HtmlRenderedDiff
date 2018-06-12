109a04a826ae5967646a28f48b51453caf7bddee
chhay menghong <menghong.chhay@jpl.nasa.gov>
2018-06-12 12:06:38 -0700

    ignore certain files for release

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports =  {
     entry: {
-        index: path.resolve(__dirname, './index'),
-        test: path.resolve(__dirname, './unitTest')
+        index: path.resolve(__dirname, './index')
     },
 
     output: {
